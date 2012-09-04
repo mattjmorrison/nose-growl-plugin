d0a703de20295948af653b35f90cdb88ffd51f4d
mattjmorrison <mattj.morrison@gmail.com>
2012-09-03 21:17:27 -0500

    added version and README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nose-growl',
-    version='0.0.6',
+    version='0.0.1',
     py_modules=['growl'],
     install_requires=['gntp', 'nose'],
     entry_points = {
